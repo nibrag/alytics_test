85c7d2c9adc21a28d2bb4b00d32488b0c9bea950
nib <nib@dvhb.ru>
2016-09-18 16:56:01 +0300

    Миграция для моделей

--- app/models.py
+++ app/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.contrib.postgres.fields import JSONField
 
 
-class Data(models.CharField):
+class Data(models.Model):
     name = models.CharField(max_length=129)
     data = JSONField()
 
 
-class ErrorLog(models.CharField):
+class ErrorLog(models.Model):
     ts = models.DateTimeField()
     error = models.TextField()
