a25fb808631bd6f6307ba0a57585550922ac511a
nib <nib@dvhb.ru>
2016-09-18 21:37:18 +0300

    placeholder для фронта

--- app/forms.py
+++ app/forms.py
@@ -5,8 +5,9 @@ from .models import Data
 
 class AddDataForm(forms.ModelForm):
     name = forms.CharField(max_length=129, label='Название данных')
-    data = forms.CharField(widget=forms.Textarea(attrs={'class': 'span8'}),
-                           label='Данные')
+    data = forms.CharField(widget=forms.Textarea(
+        attrs={'class': 'span8', 'placeholder': '[{"a": 1, "b": 1}, {"a": 2, "b": 3]'}),
+        label='Данные')
 
     def clean_data(self):
         try:
